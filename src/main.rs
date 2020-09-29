88503ca42a6ffffa7e25f6cee5631d6faf9fcbfe
haris <harismuzaffer@gmail.com>
2020-09-29 09:07:00 +0000

    commit from arch

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,78 @@ fn main(){
     // loops();
     // fibonacci(100);
     // borrowing();
-    let s = String::from("haris");
+    let mut s = String::from("haris shah");
     // ownit(s);
     // let arr = [3, 7, 8, 23, 4, 8];
     // loops2(&arr);
+    // slicetest(&s);
+    // s.clear(); we can not have more than one mutable references at a time
+    // or one mutable and one immutable. But here clear although does take
+    // mutable reference but that goes out of scope here, so we cant have a mutable
+    // reference in the next line
+    // let ss = &mut s;
+    // println!("s is {}", ss);
+    let user: User = User {
+        name: "haris".to_string(),
+        age: 26
+    };
+
+    let user2 = User {
+        name: "hammad".to_string(),
+        ..user
+    };
+
+    struct Color(u32, u32, u32);
+    let white = Color(255, 255, 255);
+    let Color(r, g, b) = white;
+    println!("white is {} {} {}", r, g, b);
+    // let j = "hello";
+    // let k = j;
+    // println!("j is {}", j);
+    // println!("k is {}", k);
+    // println!("j is {}", j);
+    let a = "hello world!";
+    let b = a; // this doesnt move ownership from a to b becoz a is a string literal
+    // string literals are references by default... let a = "" is like let a: &str = ""
+    println!("a is {}", a);
+
+    let rectangle1 = Rectangle {
+        length: 12,
+        width: 10
+    };
+
+    println!("area of {:?} is {}", rectangle1, rectangle1.area());
+
+    let ip_v4 = IpAddrKind::V4;
+    let ip_v6 = IpAddrKind::V6;
+}
+
+enum IpAddrKind {
+    V4,
+    V6
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    length: u32,
+    width: u32
+}
+
+impl Rectangle {
+    fn area(&self)-> u32 {
+        self.length * self.width
+    }
+}
+
+struct User{
+    name: String,
+    age: u32
+}
+
+
+fn slicetest(s: &String){
+   let slice = &s[0..5];
+   println!("slice is {}", slice);
 }
 
 fn loops2(arrr: &[u32]){
