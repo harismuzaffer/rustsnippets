4dd70d7918b0c632501cc274625a17982b1d8c20
haris <harismuzaffer@gmail.com>
2022-04-03 08:03:17 +0530

    add an example and comments to understand lifetimes

--- src/lifetimes.rs
+++ src/lifetimes.rs
@@ -12,6 +12,74 @@ pub fn dangling_ref() {
 
 
 pub mod life_times_and_elision_rules {
+
+    pub mod understanding_lifetimes {
+        // lifetime is all about references. If there is not reference involved, no need of
+        // lifetimes
+        
+        // When there are references involved, borrow checker sometimes is not able to infer
+        // lifetime of certain references
+        
+        // example when borrow checker is able to infer lifetimes ...
+        // let x;
+        // {
+        //      let a = 10;
+        //      z = &a;
+        //      println!("perfectly fine to print z as it refers to `a` which is still alive i.e. still lives {}", z);
+        //      `a` is dropped afret this block ends, any reference to `a` is invalid
+        // }
+        // println!("invalid to print z as it refers to `a` which doesn't live enough so that `z` 
+        //          can be further used {}", z);
+        
+        // example when borrow checker is not able to infer lifetimes, thus needs programmers help
+        // to validate lifetimes of some references...
+        // let x = 10;
+        // let result;
+        // {
+        //      let y = 20;
+        //      r = max(&x, &y); max returns the reference of the maximum of the two values passed
+        //      // compiler doesn't know whether reference of x or y would be returned, so it is
+        //      not gauranteed that the next print statement is printing a valid reference or not
+        // }
+        // println!("invalid to print result as it refers to either `x` or `y`  which in case of y doesn't live enough so that `result` 
+        //          can be further used {}", z);
+
+        fn applying_lifetimes () {
+            let string1 = String::from("long string is long");
+            let result;
+            {
+                let string2 = String::from("xyz");
+                // the fn longest is a compilation error becuase rust compiler doenst know what which string will be returned
+                // from it. if string string2 ref is returned then it would have been error of string2 not
+                // living enough.. to show that error i.e. the string returned by the fn call lives enough
+                // or not, we need to hit the compiler and tell it what is the lifetime relationship here
+                //
+                // in other words, if we dont tell compiler that lifetime relationships, it wont
+                // allow us to proceed without that and if we add lifetime relationships, the 
+                // print after this block is still an error but now it is becuase result doesn live enough to
+                // support both string1 and string2 lifetime
+                
+                result = longest(string1.as_str(), string2.as_str());
+            }
+            // println!("The longest string is {}", result);
+        } 
+
+        fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+            // The two references passed to compare have different lifetimes (or scopes).
+            // Since y has a smaller lifetime(y referes to string2 and lives only during the block
+            // above), it becomes the concrete lifetime. This means that all the parameters that 
+            // have 'a written next to them should have a lifetime at least as long as y. This also
+            // means that answer, which stores the returned reference, should be valid as long as y
+            // is valid. Therefore, if we use result out of the scope of y, we get a compilation error:
+            if x.len() > y.len() {
+                x
+            } else {
+                y
+            }
+        }
+
+    }
+
     // by applying the first and second elision rule, there is no need of explicit litetimes here.
     // the output reference is given the same lifetime as that of the input reference.
     pub fn first_word(s: &str) -> &str {
